ca10be8e00a0718beb2d505b7fa861d6ba51b619
Scott Wainstock <scott.wainstock@gmail.com>
2013-06-24 19:48:07 -0700

    secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -3,13 +3,10 @@
 if Rails.env.development? || Rails.env.test?
   Enki::Application.config.secret_token = SecureRandom.hex(20)
 else
-  # Your secret key for verifying the integrity of signed cookies.
-  # If you change this key, all old signed cookies will become invalid!
-  # Make sure the secret is at least 30 characters and all random,
-  # no regular words or you'll be exposed to dictionary attacks.
-  raise "Generate a secret with `rake secret` and paste it into `config/initializers/secret_token.rb`."
-
-  # If this is an open source project, DO NOT commit your secret to source
-  # control. Load it from ENV or a file that is git ignored (File.read)
-  Enki::Application.config.secret_token = 'paste your token here'
+  secret = ENV['APP_SECRET']
+  if secret.length < 30
+    raise "Secret token cannot be loaded"
+  else
+    Myapp::Application.config.secret_token = secret
+  end
 end
