be573e4e023f255b379326e7ebc2618c59feb196
Scott Wainstock <scott.wainstock@gmail.com>
2013-06-25 19:52:40 -0700

    updating config assets

--- config/application.rb
+++ config/application.rb
@@ -68,20 +68,10 @@ module Enki
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
-    config.assets.precompile += %w(
-      humanmsg.css
-      admin.css
-      application.css.scss
-      login.css
-      admin.js
-      application.js
-      common.js
-      live-comment-preview.js
-    )
+    config.assets.initialize_on_precompile = false
   end
 end
