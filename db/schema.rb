1dc5cf107adfa4d024e4bb64ac7537238641eae2
ryantg <gratzer@posteo.org>
2016-12-08 21:47:57 -0800

    gemfile and schema, dunno why

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150414113518) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: true do |t|
     t.integer  "post_id",      null: false
     t.string   "author",       null: false
@@ -24,8 +27,8 @@ ActiveRecord::Schema.define(version: 20150414113518) do
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["created_at"], name: "index_comments_on_created_at"
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["created_at"], name: "index_comments_on_created_at", using: :btree
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "omni_auth_details", force: true do |t|
     t.string   "provider",    null: false
@@ -46,9 +49,9 @@ ActiveRecord::Schema.define(version: 20150414113518) do
     t.datetime "updated_at"
   end
 
-  add_index "pages", ["created_at"], name: "index_pages_on_created_at"
-  add_index "pages", ["slug"], name: "pages_slug_unique_idx"
-  add_index "pages", ["title"], name: "index_pages_on_title"
+  add_index "pages", ["created_at"], name: "index_pages_on_created_at", using: :btree
+  add_index "pages", ["slug"], name: "pages_slug_unique_idx", using: :btree
+  add_index "pages", ["title"], name: "index_pages_on_title", using: :btree
 
   create_table "posts", force: true do |t|
     t.string   "title",                                  null: false
@@ -64,8 +67,8 @@ ActiveRecord::Schema.define(version: 20150414113518) do
     t.datetime "edited_at",                              null: false
   end
 
-  add_index "posts", ["published_at"], name: "index_posts_on_published_at"
-  add_index "posts", ["slug"], name: "posts_slug_unique_idx"
+  add_index "posts", ["published_at"], name: "index_posts_on_published_at", using: :btree
+  add_index "posts", ["slug"], name: "posts_slug_unique_idx", using: :btree
 
   create_table "sessions", force: true do |t|
     t.string   "session_id", null: false
@@ -74,8 +77,8 @@ ActiveRecord::Schema.define(version: 20150414113518) do
     t.datetime "updated_at"
   end
 
-  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
@@ -83,15 +86,15 @@ ActiveRecord::Schema.define(version: 20150414113518) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id"], name: "index_taggings_on_taggable_id_and_taggable_type"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  add_index "taggings", ["taggable_id"], name: "index_taggings_on_taggable_id_and_taggable_type", using: :btree
 
   create_table "tags", force: true do |t|
     t.string  "name"
     t.integer "taggings_count", default: 0, null: false
   end
 
-  add_index "tags", ["name"], name: "index_tags_on_name"
+  add_index "tags", ["name"], name: "index_tags_on_name", using: :btree
 
   create_table "undo_items", force: true do |t|
     t.string   "type",       null: false
@@ -99,6 +102,6 @@ ActiveRecord::Schema.define(version: 20150414113518) do
     t.text     "data"
   end
 
-  add_index "undo_items", ["created_at"], name: "index_undo_items_on_created_at"
+  add_index "undo_items", ["created_at"], name: "index_undo_items_on_created_at", using: :btree
 
 end
